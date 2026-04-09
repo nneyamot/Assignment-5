f25be919ae56f17a559df6530e352dc3cf972234
nneyamot <mohammadnneyamot@gmail.com>
2026-04-09 21:21:03 +0600

    modal done

--- js/index.js
+++ js/index.js
@@ -20,6 +20,44 @@ const allDivLoad = () => {
 
 }
 
+const loadTheDetail= async (id)=>{
+    const url=`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    const res=await fetch(url);
+    const details=await res.json();
+    displayTheDetails(details.data);
+}
+
+const displayTheDetails=(details)=>{
+ console.log(details);
+  const detailsContainer=document.getElementById('details-container');
+  detailsContainer.innerHTML='';
+  const detailsDiv=document.createElement('div')
+  detailsDiv.innerHTML=`
+            <div class="space-y-3">
+                    <h1 class="font-bold text-2xl">${details.title} </h1>
+                    <div class="flex gap-2 justify-left items-center text-[#64748B] ">
+                        <button class="btn h-6 rounded-lg bg-green-600 text-white">${details.status}</button>
+                        <p>Opened by the ${details.author} </p>
+                        <p>${details.updatedAt} </p>
+                    </div>
+                    <p class="text-[#64748B] text-[12px]">${details.description}</p>
+                    <div class="flex gap-30">
+                        <div>
+                            <p class="text-[#64748B]"> Assignee:</p>
+                            <h1 class="font-bold">${details.assignee}</h1>
+                        </div>
+                        <div>
+                            <p class="text-[#64748B]">priority:</p>
+                            <h1 class="high w-30 font-bold">${details.priority}</h1>
+                        </div>
+
+                    </div>
+                </div>
+  `
+  detailsContainer.append(detailsDiv);
+  document.getElementById('my_modal_5').showModal();
+}
+
 const displayCard = (items) => {
 
     const divContainer = document.getElementById('div-container');
@@ -59,6 +97,7 @@ const displayCard = (items) => {
 
         const card = document.createElement('div');
         card.innerHTML = `
+       
         <div class="shadow-md p-6 space-y-5 rounded-lg ${item.status}">
            <div class="flex justify-between items-center">
              <i class="fa-regular fa-circle"></i>
@@ -70,11 +109,10 @@ const displayCard = (items) => {
               <p class="description text-[10px] test-[#64748B] "> ${item.description}</p>
            </div>
 
-           <div class="objects flex flex-col gap-2">
-            
+           <div class="objects flex flex-col gap-2">  
             ${createElements(item.labels)}
-
            </div>
+             <button  onclick="loadTheDetail(${item.id})" class="btn btn-soft btn-accent h-6">More..</button>
            <hr class="">
            <div class="space-y-3">
             <h1 class="author text-[12px] text-[#64748B]"> ${item.author} </h1>
